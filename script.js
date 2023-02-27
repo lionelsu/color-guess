83fab3cdf61f8b85c1da87df0ae69c187b5b2d3a
lionelsu <lionel_suicune@hotmail.com>
2023-02-26 22:09:05 -0400

    req: 5 done, added functions to show a message if the color is the correct color

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Seletores de elementos
 const colorBalls = document.querySelectorAll('.ball');
+const answer = document.querySelector('#answer');
+const rgbQuestion = document.querySelector('#rgb-color');
 
 // Função para gerar um número aleatório entre 0 e 255
-const rdnRGB = () => Math.floor(Math.random() * 255);
+// maxValue como parâmetro para reutilizar a função, no caso, será 255 ou o .length do const colorBalls
+const rdnRGB = (maxValue) => Math.floor(Math.random() * maxValue);
 
 // Função para gerar e concatenar um rgb(n, n, n)
-const genRGB = () => `rgb(${rdnRGB()},${rdnRGB()},${rdnRGB()})`;
+const genRGB = () => `rgb(${rdnRGB(255)},${rdnRGB(255)},${rdnRGB(255)})`;
 
 // Função para gerar a cor aleatória nos circulos
 function applyRdnColor(nodeList) {
@@ -16,4 +20,30 @@ function applyRdnColor(nodeList) {
   //   colorBalls[i].style.backgroundColor = genRGB();
   // }
 }
-applyRdnColor(colorBalls);
+
+// Função para retornar uma das cores aleatórias geradas e renderizar apenas o seu código rgb no paragrafo. Esta cor é a cor que deve ser adivinhada.
+function getTargetBall() {
+  const indexBall = Math.floor(Math.random() * colorBalls.length);
+  const targetBall = colorBalls[indexBall].style.backgroundColor;
+  return targetBall;
+}
+
+// Função usada para aplicar as cores aleatórias e aplicar a cor a ser advinhada no HTML
+const applyColorBalls = () => {
+  applyRdnColor(colorBalls);
+  rgbQuestion.innerText = getTargetBall();
+};
+applyColorBalls();
+
+colorBalls.forEach((ball) => {
+  ball.addEventListener('click', () => {
+    const isTheBall = ball.style.backgroundColor;
+    const correctBall = rgbQuestion.textContent;
+
+    if (isTheBall === correctBall) {
+      answer.textContent = 'Acertou!';
+    } else {
+      answer.textContent = 'Errou! Tente novamente!';
+    }
+  });
+});
